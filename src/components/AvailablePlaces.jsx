c287ce41ed49a8f6630c973d60e1e221175e15bc
Kushal Niraula <kushal463334@gmail.com>
2024-04-26 22:22:08 +1000

    applied custom hook for multiple states

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,34 +1,30 @@
-import { useState } from "react";
 import Places from "./Places.jsx";
-import { useEffect } from "react";
 import Error from "./Error";
-import {sortPlacesByDistance} from "../loc.js";
-import {FetchAvailablePlaces} from "../http.js";
+import { sortPlacesByDistance } from "../loc.js";
+import { FetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../hooks/useFetch.js";
+
+async function fetchSortedPlaces() {
+  const places = await FetchAvailablePlaces();
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await FetchAvailablePlaces();
-        navigator.geolocation.getCurrentPosition((position)=>{
-          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude)
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        })
-      } catch (error) {
-        setError({
-          message: error.message || "Cant fetch places, please try again later",
-        });
-        setIsFetching(false);
-      }
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
 
-    }
-    fetchPlaces();
-  }, []);
   if (error) {
     return <Error title="An error occured..." message={error.message} />;
   } else {
